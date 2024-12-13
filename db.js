40aa0da0a2e1b67ced2f101994524aee5ab2d5b0
ゆゆゆ <yuyuyu1226.m.1226@gmail.com>
2024-12-13 20:51:18 +0900

    Create db.js

---
+++ db.js
@@ -0,0 +1,11 @@
+var { Pool } = require("pg");
+require("dotenv").config();
+
+var pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
